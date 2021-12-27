c54c6f61ca6d27af771d4c058eba4d36027d44d8
ujiik009 <ikool09@hotmail.com>
2021-12-27 18:05:50 +0700

    install socketio

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,32 @@
 // const express = require('express')
 import express, { Application, Request, Response } from "express"
 
-
+import * as socketio from 'socket.io';
 const apiService: Application = express()
-const socketService: Application = express()
-const portAPI: Number = 3000
-const portSocket: Number = 3001
+const server = require('http').Server(apiService);
 
-apiService.get('/', (req: Request, res: Response) => {
-    res.send('Hello World! API')
+const io: socketio.Server = new socketio.Server();
+io.attach(server);
+const port: Number = 3000
+
+
+
+apiService.get('/', async (_: Request, res: Response) => {
+    res.send('Hello World')
+});
+
+io.on('connection', (socket: socketio.Socket) => {
+    console.log('connection',socket.id);
+    socket.emit('status', 'Hello from Socket.io');
+
+    socket.on('disconnect', () => {
+        console.log('client disconnected',socket.id);
+    })
+});
+
+
+
+server.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
 
-socketService.get("/", (req: Request, res: Response) => {
-    res.send('Hello World! Socket')
-})
-
-apiService.listen(portAPI, () => {
-    console.log(`Example app listening at http://localhost:${portAPI}`)
-})
-
-
-
-
-socketService.listen(portSocket, () => {
-    console.log(`Example app listening at http://localhost:${portSocket}`)
-})
