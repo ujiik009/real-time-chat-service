46ef4c24215de187b591eed05c178b3f9c328567
ujiik009 <ikool09@hotmail.com>
2021-12-27 17:37:24 +0700

    install express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-console.log("Service started")
+// const express = require('express')
+import express, { Application, Request, Response } from "express"
 
+
+const apiService: Application = express()
+const socketService: Application = express()
+const portAPI: Number = 3000
+const portSocket: Number = 3001
+
+apiService.get('/', (req: Request, res: Response) => {
+    res.send('Hello World! API')
+})
+
+socketService.get("/", (req: Request, res: Response) => {
+    res.send('Hello World! Socket')
+})
+
+apiService.listen(portAPI, () => {
+    console.log(`Example app listening at http://localhost:${portAPI}`)
+})
+
+
+
+
+socketService.listen(portSocket, () => {
+    console.log(`Example app listening at http://localhost:${portSocket}`)
+})
